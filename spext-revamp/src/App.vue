c1ebc6ab7471c58dc81aff8bf01026f3ffbc9400
ameya <ameya.soman@coditas.com>
2019-04-22 15:05:39 +0530

    initialized the project

---
+++ spext-revamp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+@import url("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
+body {
+  overflow: hidden;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
