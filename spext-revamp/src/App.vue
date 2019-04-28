110f2cd0911aa69ca8c412f3f944665259bbf821
ankita.medhekar@coditas.com <ankita.medhekar@coditas.com>
2019-04-28 21:22:16 +0530

    added demo component

--- spext-revamp/src/App.vue
+++ spext-revamp/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <Header/>
-    <HelloWorld/>
+    <!-- <HelloWorld/> -->
+    <Demo></Demo>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Demo from "./components/Demo.vue";
 import Header from "./components/core/Header/Header.vue";
 export default {
   name: "app",
   components: {
     HelloWorld,
-    Header
+    Header,
+    Demo
   }
 };
 </script>
@@ -20,14 +23,14 @@ export default {
 <style>
 @import url("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
 body {
-  overflow: hidden;
+  /* overflow: hidden; */
   margin: 0;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   /* margin-top: 60px; */
 }
---
+++ spext-revamp/src/components/Demo.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div contenteditable="true" ref="todoItem">
+      <div
+        v-for="(item,i) in todos"
+        class="todo-item"
+        @keyup="updateTodos($event,i)"
+        v-bind:key="i"
+      >{{item.title}}</div>
+    </div>
+    <button @click="show()">click</button>
+  </div>
+</template>
+<style lang="scss">
+.todo-item {
+  padding: 16px;
+}
+</style>
+
+<script>
+export default {
+  name: "Demo",
+  todos: [],
+  todoItemCount: 0,
+  todoItemList: [],
+  data() {
+    return {
+      todos: [
+        {
+          title:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, dui sit amet dapibus elementum, purus urna ultricies massa, a laoreet arcu nibh non est. Fusce risus orci, pulvinar at pretium vehicula, consequat sed lectus. Aliquam erat volutpat. Morbi iaculis vel eros ac sodales. Cras ut nisl pretium, semper massa ut, laoreet eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi mauris ipsum, tincidunt in mollis ac, venenatis ac velit. Nullam posuere ac lectus vel blandit. Suspendisse potenti. Pellentesque magna lorem, placerat id ultrices id, tincidunt at odio."
+        },
+        {
+          title:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus, dui sit amet dapibus elementum, purus urna ultricies massa, a laoreet arcu nibh non est. Fusce risus orci, pulvinar at pretium vehicula, consequat sed lectus. Aliquam erat volutpat. Morbi iaculis vel eros ac sodales. Cras ut nisl pretium, semper massa ut, laoreet eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi mauris ipsum, tincidunt in mollis ac, venenatis ac velit. Nullam posuere ac lectus vel blandit. Suspendisse potenti. Pellentesque magna lorem, placerat id ultrices id, tincidunt at odio."
+        }
+      ]
+    };
+  },
+
+  methods: {
+    show() {
+      //   console.log(this.$refs);
+      //   console.log(window.getSelection());
+      this.todoItemCount = this.$refs.todoItem.childElementCount;
+      //   console.log(this.todoItemCount);
+      this.todoItemList = this.$refs.todoItem;
+      //   console.log(JSON.parse(JSON.stringify(this.todoItemList.childNodes)));
+      //   console.log(this.$refs.todoItem.childNodes);
+      //   for (let i = 0; i < this.todoItemCount; i++) {
+      //     console.log(this.$refs.todoItem.childNodes[i].innerText);
+      //     // this.todos[i].title = this.$refs.todoItem.childNodes[i].innerText;
+
+      //   }
+
+      //   console.log(this.updatedTodos);
+      //   this.todos[1].title = this.$refs.todoItem.childNodes[0].innerText;
+    }
+  }
+};
+</script>
