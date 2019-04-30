33ae730262ae3868465108fa08f39029d23016ed
Archana Bichave <abichave@tavisca.com>
2019-04-30 16:45:48 +0530

    created generic popover component

--- spext-revamp/src/components/core/Header/Header.vue
+++ spext-revamp/src/components/core/Header/Header.vue
@@ -1,31 +1,25 @@
 <template src="./Header.html"></template>
 
 <script>
+import Popover from "./../Popover/Popover";
 export default {
+  config: {},
   data() {
-    return {
-      showMenu: false,
+    return (this.config = {
+      show: false,
       leftPos: "",
       topPos: ""
-    };
+    });
+  },
+  components: {
+    Popover
   },
   methods: {
     openTranscript(event) {
-      this.showMenu = !this.showMenu;
+      this.show = !this.show;
       this.leftPos = this.$refs.editTranscript.offsetLeft + "px";
       this.topPos = this.$refs.navbar.offsetHeight - 8 + "px";
-    },
-    documentClick(event) {
-      if (
-        event.target.parentElement.className !==
-        "menu-middle anchor menu-active"
-      ) {
-        this.showMenu = false;
-      }
     }
-  },
-  created() {
-    window.addEventListener("click", this.documentClick);
   }
 };
 </script>
