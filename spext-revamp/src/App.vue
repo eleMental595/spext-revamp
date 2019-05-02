2653f02f1f0e354204c13b441a21c1f8e9aa896d
Archana Bichave <abichave@tavisca.com>
2019-05-02 11:55:34 +0530

    changes in config file and fixes in header component

--- spext-revamp/src/App.vue
+++ spext-revamp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header/>
-    <!-- <TranscribedText></TranscribedText> -->
+    <TranscribedText></TranscribedText>
     <!-- <Sounds></Sounds> -->
     <router-view></router-view>
   </div>
@@ -9,12 +9,12 @@
 
 <script>
 import Header from "./components/core/Header/Header.vue";
-// import TranscribedText from "./components/TranscribedText/TranscribedText";
+import TranscribedText from "./components/TranscribedText/TranscribedText";
 import Sounds from "./components/Sounds/Sounds";
 export default {
   name: "app",
   components: {
-    // TranscribedText,
+    TranscribedText,
     Header,
     Sounds
   }
