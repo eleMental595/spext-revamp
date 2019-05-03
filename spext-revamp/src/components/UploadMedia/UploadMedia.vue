a1166b942deeba89492fe0d22dd36eba865a5dda
ankitac <ankitamedhekar25@gmail.com>
2019-05-03 22:39:36 +0530

    changed fonts of upload media component

--- spext-revamp/src/components/UploadMedia/UploadMedia.vue
+++ spext-revamp/src/components/UploadMedia/UploadMedia.vue
@@ -1,7 +1,22 @@
 <template src="./UploadMedia.html"></template>
+import { setTimeout } from 'timers';
 <style lang="scss" src="./UploadMedia.scss"></style>
 <script>
 export default {
   name: "UploadMedia"
+  // titleName: "",
+  // uploading: false,
+  // uploadComplete: false,
+
+  // data() {
+  //   return {
+  //     titleName: "",
+  //     uploading: false,
+  //     uploadComplete: false
+  //   };
+  // },
+  // methods: {
+
+  // }
 };
 </script>
